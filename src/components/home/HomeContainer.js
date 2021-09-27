789af15d092f6bb98a8103347eb43b7f0563ca46
Zhiyu <zawlinnnaing0018@gmail.com>
2021-09-27 15:00:35 +0630

    - refactor under constrution card

--- src/components/home/HomeContainer.js
+++ src/components/home/HomeContainer.js
@@ -1,6 +1,6 @@
 import { Transition } from "@headlessui/react";
 import React, { Fragment, useEffect, useState } from "react";
-import UnderConstruction from "../../images/under-construction.svg";
+import UnderConstructionCard from "../common/UnderConstructionCard";
 
 export default function HomeContainer() {
   const [isShowing, setIsShowing] = useState(false);
@@ -21,20 +21,10 @@ export default function HomeContainer() {
           entered="top-1/4"
           leave="transform duration-200 transition ease-in-out"
           leaveFrom="opacity-100 top-1/4 "
-          leaveTo="opacity-0 -top-1/4 "
+          leaveTo="opacity-0 -top-1/4"
         >
-          <div className="relative inline-block">
-            <figure className="dark:bg-gray-100 bg-gray-800 rounded p-8 shadow-md text-center max-h-250px">
-              <img
-                className="w-80 h-auto rounded-full mx-auto -top-32 relative bg-white p-4 dark:bg-gray-900"
-                src={UnderConstruction}
-                alt="Under construction"
-              />
-              <p className="dark:text-black text-lg font-bold text-white mt-8 -top-32 relative">
-                This site is under construction. Please check back again soon
-                😉.
-              </p>
-            </figure>
+          <div className="relative inline-block top-1/4">
+            <UnderConstructionCard bodyText="This site is under construction please check back soon later. 😉" />
           </div>
         </Transition>
       </div>
