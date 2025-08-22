6cdcf6a1eb2a8e9b87ec67ab261754e09504223e
Zaw <zawlinnnaing0018@gmail.com>
2025-08-22 11:52:34 +0700

    feat: added more Sertis Experience

--- src/components/experience/SertisSummary.tsx
+++ src/components/experience/SertisSummary.tsx
@@ -22,6 +22,11 @@ export default function SertisSummary() {
             became a go-to person for the team in the domain.
           </SummaryListItem>
           <SummaryListItem>
+            Designed and developed a large language model (LLM) conversational
+            AI platform by integrating in-house AI services, adopted internally
+            by a leading bank in Thailand.
+          </SummaryListItem>
+          <SummaryListItem>
             Wrote frontend best practices and guidelines for the team to follow.
           </SummaryListItem>
         </SummaryList>
@@ -29,6 +34,16 @@ export default function SertisSummary() {
       <SummarySection>
         <SummaryTitle title="CHALLENGES" />
         <SummaryList>
+          <SummaryListItem>
+            Managed frequently changing client requirements and consistently
+            delivered solutions within accelerated timelines.
+          </SummaryListItem>
+          <SummaryListItem>
+            Faced with a monolithic legacy codebase causing high CPU usage,
+            re-architected the system into modular components and applied
+            specialized technologies for each, achieving 80% improvement in
+            performance and resource efficiency.
+          </SummaryListItem>
           <SummaryListItem>
             Shared technical knowledge within the team and enabled team members
             to be up-to-date with the latest industry best practices.
